64e623f6384c48ad7b6ab62be724914770334792
Kristy Lavache <kristy.lavache@gmail.com>
2019-03-13 22:54:40 +0000

    Fixed colour on form page and added input sections and submit button

--- flask_app.py
+++ flask_app.py
@@ -5,5 +5,16 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
+@app.route('/form', methods=['GET'])
+def form():
+    return render_template("form.html")
+
+def data():
+    file_object  = open("data.txt", "r")
+    line = file_object.readline()
+    info = line.split()
+    print(info)
+
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    data()
